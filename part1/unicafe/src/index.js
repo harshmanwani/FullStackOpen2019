917e339369cd2c1f8b45667f2c0a24c5e7aa243f
Harsh Manwani <invokerharsh@gmail.com>
2019-11-06 14:48:23 +0530

    part1_unicafe_step4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,26 +2,30 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({ good, neutral, bad }) => {
 
   const all = good + neutral + bad;
 
   const getAverage = () => (good - bad) / all;
   const getPositive = () => (good / all) * 100;
 
-  return (
-    <div>
-      <h2>Statistics</h2>
+  if ((good || bad || neutral) !== 0) {
+    return (
       <div>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <p>All: {all}</p>
-        <p>Average: {getAverage() || 0}</p>
-        <p>Positive: {getPositive() || 0}</p>
+        <h2>Statistics</h2>
+        <div>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>All: {all}</p>
+          <p>Average: {getAverage() || 0}</p>
+          <p>Positive: {getPositive() || 0}</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  return <div>No Feedback Given!</div>
 }
 
 const App = () => {
@@ -38,7 +42,7 @@ const App = () => {
         <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
         <button onClick={() => setBad(bad + 1)}>Bad</button>
       </div>
-      <Statistics 
+      <Statistics
         good={good}
         neutral={neutral}
         bad={bad}
