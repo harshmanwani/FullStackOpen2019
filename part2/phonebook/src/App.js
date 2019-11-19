cf0eed13575d3aba393a0deddf6112612cafb192
Harsh Manwani <invokerharsh@gmail.com>
2019-11-19 11:41:56 +0530

    part2 phonebook step3: Add phone numbers also

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,40 +1,39 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('')
-  const addNewName = (e) => {
+  const [persons, setPersons] = useState([]);
+  const [formData, setformData] = useState({ name: '', number: '' })
+
+  const addPerson = (e) => {
     e.preventDefault();
-    if (persons.some(person => person.name === newName)) {
-      window.alert(`${newName} is already added to the phonebook.`);
+    if (persons.some(person => person.name === formData.name)) {
+      window.alert(`${formData.name} is already added to the phonebook.`);
       return;
     }
-    setPersons(persons.concat({ name: newName }));
-    setNewName('');
+    setPersons(persons.concat(formData));
+    setformData({ name: '', number: '' });
   }
-  const handleNameChange = (e) => {
-    setNewName(e.target.value);
+
+  const handleFormInput = (e, type) => {
+    const currentData = { ...formData };
+    currentData[type] = e.target.value
+    setformData(currentData);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNewName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={formData.name} onChange={e => handleFormInput(e, 'name')} /></div>
+        <div>number: <input type="number" value={formData.number} onChange={e => handleFormInput(e, 'number')} /></div>
+        <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => <p>{person.name}</p>)
+        persons.map(person => <p>{person.name} {person.number}</p>)
       }
     </div>
   )
 }
 
-export default App
+export default App;
