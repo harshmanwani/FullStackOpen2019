79fd0167eeb8f2e5c9bec0f604d9c2aa8dac3285
Harsh Manwani <invokerharsh@gmail.com>
2019-11-19 12:28:43 +0530

    part2 phonebook step4: implement search filter

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
   const [formData, setformData] = useState({ name: '', number: '' })
+  const [filter, setFilter] = useState('');
 
   const addPerson = (e) => {
     e.preventDefault();
@@ -12,17 +18,22 @@ const App = () => {
     }
     setPersons(persons.concat(formData));
     setformData({ name: '', number: '' });
-  }
+  };
 
   const handleFormInput = (e, type) => {
     const currentData = { ...formData };
     currentData[type] = e.target.value
     setformData(currentData);
-  }
+  };
+
+  const handleFilter = e => {
+    setFilter(e.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={filter} onChange={handleFilter} /></div>
       <form onSubmit={addPerson}>
         <div>name: <input value={formData.name} onChange={e => handleFormInput(e, 'name')} /></div>
         <div>number: <input type="number" value={formData.number} onChange={e => handleFormInput(e, 'number')} /></div>
@@ -30,7 +41,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => <p>{person.name} {person.number}</p>)
+        persons.map((person, key) => {
+          if (person.name.toLowerCase().includes(filter.toLowerCase()))
+            return <p key={key}>{person.name} {person.number}</p>
+        })
       }
     </div>
   )
