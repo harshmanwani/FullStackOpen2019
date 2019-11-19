86f05bbb74a6698a40fb85e6d6b37b3c9e53b5cb
Harsh Manwani <invokerharsh@gmail.com>
2019-11-19 11:24:09 +0530

    part2 phonebook step1: Adding names

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+  const addNewName = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+  }
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {
+        persons.map(person => <p>{person.name}</p>)
+      }
+    </div>
+  )
+}
+
+export default App
