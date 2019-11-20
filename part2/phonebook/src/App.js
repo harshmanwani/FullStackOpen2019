ce0ef7fdf425e5ef216b8b1f47e4c1041957a043
Harsh Manwani <invokerharsh@gmail.com>
2019-11-20 12:47:10 +0530

    2.11: Phonebook Step6: Fetching inital persons data from json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [formData, setformData] = useState({ name: '', number: '' })
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3030/persons')
+      .then(response => setPersons(response.data))
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
