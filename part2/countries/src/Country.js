0c78dca5c6ef2166feaef1268cd7b8ef47a63bc7
Harsh Manwani <invokerharsh@gmail.com>
2019-11-25 15:46:58 +0530

    2.14: Countries step3: Show weather for country capitals

--- part2/countries/src/Country.js
+++ part2/countries/src/Country.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { WEATHERSTACK_API_KEY } from './constants';
 
 const Country = ({ data }) => {
+  const [weather, setWeather] = useState();
+  const apiParams = {
+    access_key: WEATHERSTACK_API_KEY,
+    query: data.capital
+  }
+  useEffect(() => {
+    axios
+      .get("http://api.weatherstack.com/current", { params: apiParams })
+      .then(response => setWeather(response.data.current))
+  }, []);
   return (
     <div>
       <h2>{data.name}</h2>
@@ -12,7 +24,17 @@ const Country = ({ data }) => {
           data.languages.map((lang, key) => <li key={key}>{lang.name}</li>)
         }
       </ul>
-      <img src={data.flag} alt={`Flag of ${data.name}`} width="20%" height="20%"/>
+      <img src={data.flag} alt={`Flag of ${data.name}`} width="20%" height="20%" />
+      <h2>Weather in {data.capital}</h2>
+      {
+        weather
+        ? <div>
+            <p><strong>Temperature: </strong>{weather.temperature} °C ({weather.weather_descriptions[0]})</p>
+            <img src={weather.weather_icons[0]} alt="Weather Icon" />
+            <p><strong>Wind:</strong> {weather.wind_speed} kph, Direction {weather.wind_dir}</p>
+        </div>
+        : <div>Loading...</div>
+      }
     </div>
   );
 };
